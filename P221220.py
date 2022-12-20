6588fcb330b3abbbda393081138cf6feb68f10ac
rlaxotksdla <rlaxotksdla@gmail.com>
2022-12-20 23:08:03 +0900

    221220

--- P221220.py
+++ P221220.py
@@ -3,4 +3,18 @@ from P221220_Calculator import Calculator  #자바는 파일명과 클래스명 
 import 및as 문을 사용하여 다른 파일에서 Python 가져 오기 클래스
 sys.path.insert()메서드를 사용하여 다른 파일에서 Python 가져 오기 클래스'''  
 
-cal =Calculator()
+cal =Calculator('이름')
+cal.newName("새로운이름")
+cal.powerOn()
+cal.add(5,10)
+cal.multiple(5,5)
+cal.divide(5,7)
+cal.sum(89,10,30,25,98,76,56)
+cal.batteryCheck()
+cal.divide(5,7)
+cal.divide(5,7)
+cal.divide(5,7)
+cal.batteryCheck()
+cal.powerOn()
+cal.charge()
+cal.batteryCheck()
--- P221220_Calculator.py
+++ P221220_Calculator.py
@@ -11,127 +11,89 @@ def randomName():
   return result
 
 class Calculator:
-    power=False
-    batteryMax=10
-    battery=5
-    def __init__(self,name):
-     if name=="":
-        print("계산기의 이름이 지정되지 않았습니다. 랜덤으로 생성됩니다.")
-        self._name=randomName()
-        print(f'생성된 이름은 {self._name} 입니다.')
+    def __init__(self,namedata):
+     self.power=False
+     self.batteryMax=10
+     self.battery=5
+     self.name=""
+     if namedata=="":          #이부분 원래는 Calculator()이렇게 호출하고 싶었는데 안되서 ""로 바꿈 자바는 오버로딩 이용해서 매개변수 입력안하고도 생성이가능한데 아직 방법을 못찾음 ㅠ 처음에 매개변수 이름을 안주고 나중에 메소드로 이름 입력하게 할수는있지만..
+        print("계산기의 이름이 지정되지 않았습니다. 랜덤으로 생성됩니다.") 
+        self.name=randomName()
+        print(f'생성된 이름은 {self.name} 입니다.')
         
      else:
-        self._name=name
-        print(f'계산기가 생성되었습니다. 이름은 {self._name} 입니다.')
-    def name():
-      return self._name
-    def newName(newname):
-      self._name=newname
-      print(f'계산기의 새로운 이름은 {self._name} 입니다.')
+        self.name=namedata
+        print(f'계산기가 생성되었습니다. 이름은 {self.name} 입니다.')
+    def calcName(self):
+      return self.name
+    def newName(self,newname):
+      self.name=newname
+      print(f'계산기의 새로운 이름은 {self.name} 입니다.')
+    def powerOn(self):
+      if self.power and self.battery>0:
+        print("전원이 이미 켜져있습니다.")
+      elif self.battery<=0:
+        print("배터리가 없습니다.")
+      else:
+        print("전원을 켰습니다.")
+        self.power=True
     
-
-# 		return generatedString;
-# 	}
-#     public J221220_Calculator(String a) {
-#         this.name=a;
-#         System.out.println("생성된 계산기의 이름은 "+name+" 입니다.");
-#         }
-# 	void powerOn() {
-# 		if (this.power&& this.battery>0) {
-# 			System.out.println("전원이 이미 켜져있습니다.");
-# 		}
-# 		else if(this.battery<=0) {
-# 			System.out.println("배터리가 없습니다.");		
-# 		}
-# 		else {
-# 		System.out.println("전원을 켰습니다.");
-# 		this.power=true;
-# 		}
-# 	}
-# 	void powerOff() {
-# 		if (this.power) {
-# 		System.out.println("전원을 종료합니다.");
-# 		this.power=false;
-# 		}
-# 		else {
-# 	    System.out.println("전원이 이미 꺼져있습니다.");
-# 		}
-# 	}
-# 	void charge() {
-# 		System.out.println("배터리를 충전하였습니다.");
-# 		this.battery=this.batteryMax;
-# 	}
-# 	void batteryCheck() {
-# 		System.out.println("현재 배터리는 "+(int)((double)this.battery/this.batteryMax*100)+"% 입니다.");
-# 	}
-
-# 	int add(int x,int y) {
-# 		if (this.power) {
-# 			this.battery--;
-# 			System.out.println("계산 결과는 "+(x+y)+ "입니다. 결과값이 반환됩니다.");
-# 			return x+y;
-# 		}
-# 		else if(this.battery<=0) {
-# 			System.out.println("배터리가 없습니다.");		
-# 			this.powerOff();
-# 			return 0;		
-# 		}
-# 		else {
-# 			System.out.println("먼저 전원을 켜주세요.");
-# 			return 0;
-# 		}
-# 	}
-# 	int multiple(int x,int y) {
-# 		if (this.power&& this.battery>0) {
-# 			this.battery--;
-# 			System.out.println("계산 결과는 "+(x*y)+ "입니다. 결과값이 반환됩니다.");
-# 			return x*y;
-# 		}
-# 		else if(this.battery<=0) {
-# 			System.out.println("배터리가 없습니다.");		
-# 			this.powerOff();
-# 			return 0;		
-# 		}
-# 		else {
-# 			System.out.println("먼저 전원을 켜주세요.");
-# 			return 0;
-# 		}
-# 	}
-# 	double divide(double x, double y) {
-# 		if (this.power && this.battery>0) {
-# 			this.battery--;
-# 			System.out.println("계산 결과는 "+(x/y)+ "입니다. 결과값이 반환됩니다.");
-# 			return x/y;
-# 		}
-# 		else if(this.battery<=0) {
-# 			System.out.println("배터리가 없습니다.");		
-# 			this.powerOff();
-# 			return 0;		
-# 		}
-# 		else {
-# 			System.out.println("먼저 전원을 켜주세요.");
-# 			return 0;
-# 		}
-# 	}
-# 	int adds(int ...values)  {// 혹은(int[] values)   -매개변수의 갯수가 정해져있지 않을때
-# 		if (this.power&& this.battery>0) {
-# 			this.battery--;
-# 			int sum=0;
-# 			for (int value:values) {
-# 				sum+=value;
-# 			}
-# 			System.out.println("계산 결과는 "+sum+ "입니다. 결과값이 반환됩니다.");
-# 			return sum;
-# 		}
-# 		else if(this.battery<=0) {
-# 			System.out.println("배터리가 없습니다.");		
-# 			this.powerOff();
-# 			return 0;		
-# 		}
-# 		else {
-# 			System.out.println("먼저 전원을 켜주세요.");
-# 			return 0;
-# 		}
-# 	}
-	
-# }
+    def powerOff(self):
+      if self.power:
+        print("전원을 종료합니다.")
+        self.power=False
+      else:
+        print("전원이 이미 꺼져있습니다.")
+    def charge(self):
+      print("배터리를 충전하였습니다.")
+      self.battery=self.batteryMax
+    def batteryCheck(self):
+      print(f'현재 배터리는 {int(self.battery/self.batteryMax*100)}% 입니다.')
+    def add(self,num1,num2):
+      if self.power and self.battery>0:
+        self.battery=self.battery-1
+        print(f'계산 결과는 {num1+num2}입니다. 결과값이 반환됩니다.')
+        return num1+num2
+      elif self.battery<=0:
+        print("배터리가 없습니다.")
+        if self.power:
+         self.powerOff()
+      else:
+        print("먼저 전원을 켜주세요.")
+    def multiple(self,num1,num2):
+      if self.power and self.battery>0:
+        self.battery=self.battery-1
+        print(f'계산 결과는 {num1*num2}입니다. 결과값이 반환됩니다.')
+        return num1*num2
+      elif self.battery<=0:
+        print("배터리가 없습니다.")
+        if self.power:
+         self.powerOff()
+      else:
+        print("먼저 전원을 켜주세요.")
+    def divide(self,num1,num2):
+      if self.power and self.battery>0:
+        self.battery=self.battery-1
+        print(f'계산 결과는 {num1/num2}입니다. 결과값이 반환됩니다.')
+        return num1/num2
+      elif self.battery<=0:
+        print("배터리가 없습니다.")
+        if self.power:
+         self.powerOff()
+      else:
+        print("먼저 전원을 켜주세요.")
+    def sum(self,*nums):
+      if self.power and self.battery>0:
+        self.battery=self.battery-1
+        sumdata=0
+        for i in nums:
+          sumdata=sumdata+i
+        print(f'계산 결과는 {sumdata}입니다. 결과값이 반환됩니다.')
+        return sumdata
+      elif self.battery<=0:
+        print("배터리가 없습니다.")
+        if self.power:
+         self.powerOff()
+      else:
+        print("먼저 전원을 켜주세요.")
+      
